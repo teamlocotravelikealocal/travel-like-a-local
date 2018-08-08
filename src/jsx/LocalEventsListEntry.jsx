8fb0945f92e3f1d78176347f4349a97b48ebea6b
Pooja <pooja.subramanian3@gmail.com>
2018-08-08 16:38:44 -0700

    replaced gif with a bg image- scroll works

--- src/jsx/LocalEventsListEntry.jsx
+++ src/jsx/LocalEventsListEntry.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ajaxHandler from '../../lib/ajaxHandler.js';
+import { Image, List, Icon, Label, Grid } from 'semantic-ui-react';
+
 
 class LocalEventsListEntry extends React.Component {
   constructor(props) {
@@ -35,11 +37,6 @@ class LocalEventsListEntry extends React.Component {
   // }
 
   render() {
-    // let imgSrc = this.state.imageLinks[0] || 'https://photos.smugmug.com/Other/Madurai-Meenakshi-Amman-Temple/i-wdGtMtV/0/a96afd1a/X4/IMG_8216_HDR-X4.jpg';
-    // let imgSize = {
-    //   width:'100px',
-    //   height: '100px'
-    // };
     let event = JSON.parse(this.props.event);
     return (
       <li>
@@ -52,16 +49,8 @@ class LocalEventsListEntry extends React.Component {
         <h4> {event.localtime} </h4>
         <h4> {event.free ? 'Free' : '$'} </h4>
       </li>);
-    // return (
-    //   <li className="suggestion-list-entry">
-    //     <div className="suggestion-list-entry-title">
-    //       <a href={this.props.suggestion.suggestionLink} target={this.props.suggestion.target}>{this.props.suggestion.suggestionName}</a>
-    //       <h4> Recommended by  {this.props.suggestion.suggestionSource} </h4>
-    //       <a href = {imgSrc} target='#'> <img src = {imgSrc} style={imgSize} /> </a>
-    //     </div>
-    //   </li>
-    // );
   }
 }
+
 
 export default LocalEventsListEntry;
